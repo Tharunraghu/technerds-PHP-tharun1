4445e23d41e96ae8ccc214978bad0692ea659f48
Tharun <dhushyanthsh@gmail.com>
2023-10-26 16:23:41 +0530

    26-10-23

---
+++ commands.php
@@ -0,0 +1,14 @@
+<?php
+$a="technerds";
+echo("$a");
+/* echo"tharun";
+print"$a<br>";
+
+echo"$a<br>";
+*/
+print"technerds"."<br>".$a;
+echo
+print"technerds"."<br>".$a;
+print"technerds"."<br>".$a;
+echo("technerds"."<br>".$a);
+?>
